0388f9451c9b1630a0bbe173092a14d25f1927fe
Adeboss <ademolaadeigbe3@gmail.com>
2024-11-03 23:58:26 +0100

    making the logo responsive

--- app/page.tsx
+++ app/page.tsx
@@ -28,8 +28,8 @@ export default function Home() {
   };
 
   return (
-    <div className="bg-gray-50 h-screen flex flex-col items-center justify-center">
-      <div className="font-medium text-black mt-0 flex">AdeForcast</div>
+    <div className="bg-gray h-screen flex flex-col items-center justify-center">
+      <div className="font-medium text-black mt-0 flex absolute mb-100">AdeForcast</div>
       <div className="p-8 rounded-lg flex gap-10">
         <input
           type="text"
